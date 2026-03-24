c5f441df33ed71de4558dc4abf0ec8450a877934
Potatomaster <lowratti@gmail.com>
2026-03-24 19:02:10 +0300

    updated prject

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from fastapi import Request
 from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="templates/site-info"), name="static")
-templates = Jinja2Templates(directory="templates/site-info")
+app.mount("/static", StaticFiles(directory="static/"), name="static")
+templates = Jinja2Templates(directory="templates/")
 
 @app.get("/")
 async def index(request: Request):
